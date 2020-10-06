1de3c1f1a4de653a51afb579d6ec54f69b2f4a8d
surya <62547559+gsteja025@users.noreply.github.com>
2020-10-06 18:30:24 +0530

    Update Findthemiddleofagivenlinkedlist.cpp

--- Findthemiddleofagivenlinkedlist.cpp
+++ Findthemiddleofagivenlinkedlist.cpp
@@ -60,7 +60,10 @@ void printList(struct Node *ptr)
 // Driver Code 
 int main()  
 {  
-    // Start with the empty list  
+    // Start with the empty list
+    
+    //cin_tie(false)
+    ios_base::sync_with_stdio(false);
     struct Node* head = NULL;  
       
     // Iterate and add element  
